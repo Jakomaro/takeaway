b161b22fc03ebe766addc33ef65d89f1a0255e73
jakomaro <giacomo_mauro_perticone@yahoo.it>
2025-03-26 05:59:43 +0000

    [green] added PostOrder

--- internal/api/order.go
+++ internal/api/order.go
@@ -1,1 +1,8 @@
 package api
+
+import "net/http"
+
+func PostOrder(w http.ResponseWriter, r *http.Request) {
+
+	w.WriteHeader(http.StatusCreated)
+}
--- internal/api/order_test.go
+++ internal/api/order_test.go
@@ -1,20 +1,25 @@
 package api
 
 import (
+	"net/http"
 	"net/http/httptest"
 	"strings"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestOrderService(t *testing.T) {
 
 	tests := []struct {
-		name string
-		body string
+		name       string
+		body       string
+		wantStCode int
 	}{
 		{
-			name: "success",
-			body: "",
+			name:       "success",
+			body:       "",
+			wantStCode: http.StatusCreated,
 		},
 	}
 
@@ -24,6 +29,8 @@ func TestOrderService(t *testing.T) {
 			w := httptest.NewRecorder()
 			r := httptest.NewRequest("POST", "/order", strings.NewReader(tc.body))
 			PostOrder(w, r)
+
+			assert.Equal(t, tc.wantStCode, w.Code)
 
 		})
 	}
